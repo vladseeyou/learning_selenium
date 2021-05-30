315829b61d2021fc61c2cc097efaf4e203638e9d
vladseeyou <43914083+vladseeyou@users.noreply.github.com>
2021-05-30 17:49:16 +0300

    Update test_items.py
    fixed spaces

--- test_items.py
+++ test_items.py
@@ -6,9 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_exist_add_to_cart_button(browser):
-	# переход на нужную страницу
+    # переход на нужную страницу
     browser.get(link)
     #time.sleep(30)
-	# поиск кнопки добавления товара в корзину
+    # поиск кнопки добавления товара в корзину
     WebDriverWait(browser, 3).until(
-		EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No add to cart button on page")
+        EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")), "No add to cart button on page")
