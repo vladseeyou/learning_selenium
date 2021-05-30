eadb32e62265e5bc033fde11d1b047d47303545b
vladseeyou <43914083+vladseeyou@users.noreply.github.com>
2021-05-30 17:49:53 +0300

    Update conftest.py
    fixed

--- conftest.py
+++ conftest.py
@@ -1,13 +1,13 @@
-import pytest 
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-	
+
 def pytest_addoption(parser):
 	# задаем опцию для запуска "браузер": если не указать, то запускается Chrome
 	parser.addoption('--browser_name', action='store', default='chrome', help="Choose browser: firefox or chrome")
 	# задаем опцию для запуска "язык": ru, fr и тд.
 	parser.addoption('--language', action='store', default=None, help="Say language name to select")
-	
+
 @pytest.fixture(scope="function")
 def browser(request):
 
@@ -21,7 +21,7 @@ def browser(request):
 		opts.add_experimental_option('w3c', False)
 		browser = webdriver.Chrome(options=opts)
 	elif browser_name == "firefox":
-	    # инициализируем браузер firefox с нужными опциями
+		# инициализируем браузер firefox с нужными опциями
 		fp = webdriver.FirefoxProfile()
 		fp.set_preference("intl.accept_languages", choosen_language)
 		browser = webdriver.Firefox(firefox_profile=fp)
